7934429f6074f852ed503bc619dee6a62faaa886
francois joubard <fjoubard@gmail.com>
2025-02-12 15:14:09 +0000

    commit final

---
+++ macros/functions.sql
@@ -0,0 +1,4 @@
+{% macro margin_percent(revenue, purchase_cost) %}
+round(safe_divide({{ revenue }} - {{ purchase_cost }}, {{ revenue }}),2) as margin_percent
+{% endmacro %}
+
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,13 @@
 
 with temp as (
  select *
+ , CAST((quantity * purchase_price) AS FLOAT64) AS purchase_costs
  from {{ ref("stg_raw__sales")}}
  left join {{ ref("stg_raw__product")}}
  using(products_id)
 )
 select 
 *
-, CAST((quantity * purchase_price) AS FLOAT64) AS purchase_cost
 , Round(revenue - (quantity * purchase_price),2) as margin
+, {{margin_percent('revenue','purchase_costs')}}
 from temp
