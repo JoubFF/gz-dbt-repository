87c5b2ea8a2080797ea77f7a22497d5f04eab965
francois joubard <fjoubard@gmail.com>
2025-02-12 10:46:46 +0000

    version avec mart en table

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -1,4 +1,4 @@
- SELECT
+  SELECT
      date_date
      ,COUNT(orders_id) AS nb_transactions
      ,ROUND(SUM(total_revenue),0) AS revenue
@@ -13,3 +13,7 @@
  FROM {{ref("int_orders_operational")}}
  GROUP BY  date_date
  ORDER BY  date_date DESC
+
+
+
+
